41e0050eb4907bf71f97862065d9daec2f295ac3
inclyped <shlolitsme@gmail.com>
2024-02-23 13:47:17 +0700

    Update task11.py
    Updated Task 11 to match the checkpoint file.

--- BREAKDOWN/task11.py
+++ BREAKDOWN/task11.py
@@ -1,158 +1,124 @@
-import unittest
-
-class TestAllFunctions(unittest.TestCase):
-    """
-    def test_set_character(self):
-        with self.subTest():
-            self.assertEqual(set_character_number(), 1)
-        with self.subTest():
-            self.assertEqual(set_character_number(), 2)
-        with self.subTest():
-            self.assertEqual(set_character_number(), 3)
-        with self.subTest():
-            self.assertEqual(set_character_number(), 4)
-        with self.subTest():
-            self.assertEqual(set_character_number(), 5)
-    """
-    """
-    def test_manual(self):
-        self.assertEqual(set_names_manual(2), ['WOOI KING', 'HUI XUAN'])
-        self.assertEqual(set_names_manual(3), ['HOMELANDER', 'DORAEMON', 'TERMINATOR'])
-        self.assertEqual(set_names_manual(3), ['JOEL', 'STEVEN', 'TOMMY'])
-    """
-    """
-    def test_div(self):
-        with self.subTest():
-            self.assertEqual(set_divided_by_integer(), 1)
-        with self.subTest():
-            self.assertEqual(set_divided_by_integer(), 2)
-        with self.subTest():
-            self.assertEqual(set_divided_by_integer(), 3)
-        with self.subTest():
-            self.assertEqual(set_divided_by_integer(), 4)
-        with self.subTest():
-            self.assertEqual(set_divided_by_integer(), 5)
-        with self.subTest():
-            self.assertEqual(set_divided_by_integer(), 6)
-    """
-    def test_auto(self):
-        self.assertEqual(set_names_automatic(DEFAULT_CHARACTER_LIST, 4, 5), ['LELO MUSK', 'SUPERMARIO', 'MICHONNE', 'WOLVERINE'])
-        self.assertEqual(set_names_automatic(DEFAULT_CHARACTER_LIST, 5, 3), ['HOMELANDER', 'TERMINATOR', 'MR. MONASH', 'HERMIONE GRANGER', 'SUNFLOWER'])
-    
-    
 DEFAULT_CHARACTER_LIST = ['HOMELANDER', 'IMAN', 'TERMINATOR', 'LELO MUSK', 'MR. MONASH', 
 'FYP STUDENT 2', 'HERMIONE GRANGER', 'SUPERMARIO', 'SUNFLOWER', 'THANOS',
 'LUIGI', 'CHATGPT5', 'PATRICK STAR', 'GARY THE SNAIL', 'MICHONNE',
 'VENOM','SPONGEBOB', 'DORAEMON', 'WOLVERINE', 'DORAEMON 2.0', 'BUMBLEBEE']
 
-def set_character_number() -> str:
+def set_character_number() -> int:
     """
-    Asks the user for how many characters they want, and then print an appropriate string in the end.
+    Description:
+        The function will ask for how many characters the user wants, and then printing an introduction to
+        the players. The function will also return the number of characters in the game.
+        
+    Returns:
+        @return int: The number of characters deployed in BERYL world.
+    """
+    numOfCharacters = int(input( "How many characters do you want?\n" ))
+     
+    # We will only want 1-5 characters; incorrect input will result in the user being reprompted to enter
+    while ( numOfCharacters < 1 or numOfCharacters > 5 ):
+        numOfCharacters = int(input( "You may choose 1-5 characters only!\nHow many characters do you want?\n" ))
     
-    @return: A string with an appropriate sentence. (see the output for example)
-    """
-    # asking the number of characters they want.
-    numOfCharacters = int(input("How many characters do you want?\n"))
-
-    # code repeats until the user enters the number of characters between 1 to 5 (inclusive) only.
-    while(numOfCharacters < 1 or numOfCharacters > 5):
-        # reprompts the user to enter the appropriate number again.
-        numOfCharacters = int(input("You may choose 1-5 characters only!\nHow many characters do you want?\n"))
-
-    # manipulates the string if there are multiple characters.
-    isMultipleCharacters = "characters" if numOfCharacters > 1 else "character"
-
-    # final output.
-    print ("There will be {0} {1} attempting to Rise from the (Mon)Ashes!".format(numOfCharacters, isMultipleCharacters))
+    # Need to print x Characters if x > 1, else print x Character
+    hasMultipleCharacters = "character" if numOfCharacters == 1 else "characters"
+    
+    print(f"There will be {numOfCharacters} {hasMultipleCharacters} attempting to Rise from the (Mon)Ashes!")
     return numOfCharacters
 
-def set_names_manual(number_of_characters: int) -> list[str]:
+def set_names_manual( numberOfCharacters: int ) -> list[str]:
     """
-    Returns a list of manually entered names for the characters.
+    Description:
+        The function will return a list of manually entered names for the characters that enter
+        the BERYL world.
     
-    @param int number_of_characters: The number of characters that the user wants to name.
-    @return: A list of names that the user manually entered, given that each name is unique.
-    """
-    
-    names = [] # names list to store the names.
-    
-    for i in range(number_of_characters):
-        nameToAppend = input("Set character {0} name:\n".format(i+1)).upper() # converts the string all to uppercase.
+    Parameter:
+        @param int numberOfCharacters: The number of characters that enter the BERYL world.
         
-        # repeatedly reprompts the user to enter unique names only.
+    Returns:
+        @return list[str]: A list of names that are given to the characters.
+    """
+    
+    names = [] # A list to store the names of the characters
+    
+    for i in range(numberOfCharacters):
+        nameToAppend = input(f"Set character {i+1} name:\n").upper() # Converts all of the string name to uppercase
+        
+        # If a name repeats the program will reprompt to enter an unique name for every character
         while (nameToAppend in names):
-            print("{0} is a duplicate, try again!".format(nameToAppend))
-            nameToAppend = input("Set character {0} name:\n".format(i+1)).upper()
-            
+            print(f"{nameToAppend} is a duplicate, try again!")
+            nameToAppend = input(f"Set character {i+1} name:\n").upper()
+        
         names.append(nameToAppend)
         
-        # stopping the loop if the number of character limit has been reached.
-        if len(names) == number_of_characters:
-            break
     return names
 
 def set_divided_by_integer() -> int:
     """
-    Asks the user to enter an integer between 1 and 6 (inclusive), and returns the value needed for naming characters automatically.
+    Description:
+        The function asks the user to enter an integer between 1 to 6 (inclusive), and returns that integer neeeded
+        for automatic naming. 
     
-    @return: An integer that will be used in automatic naming algorithm.
+    Returns:
+        @return int: An integer that is used for automatic naming system.
     """
-    divisible_by = int(input("Pick a number between 1-6 (inclusive):\n"))
     
-    # reprompts the user to enter the correct value within the range 1-6.    
-    while(divisible_by < 1 or divisible_by > 6):
+    divisible_by = int( input( "Pick a number between 1-6 (inclusive):\n" ) )
+    
+    # Reprompts the user to enter again if the integer is out of the range [1,6]
+    while( divisible_by < 1 or divisible_by > 6 ):
         print("You may choose a number between 1-6 (inclusive) only!")
-        divisible_by = int(input("Pick a number between 1-6 (inclusive):\n"))
+        divisible_by = int( input( "Pick a number between 1-6 (inclusive):\n" ) )
     
     return divisible_by
 
-def set_names_automatic(character_list: list[str], number_of_characters: int, divisible_by: int) -> list[str]:
+def set_names_automatic( characterList: list[str], numberOfCharacters: int, divisibleBy: int ) -> list[str]:
     """
-    Automatically appends names of the characters in a list that depends on the divisible_by integer the user has provided.
-    
-    @param list[str] character_list: A list of character names the characters can possibly have.
-    @param int number_of_characters: The number of characters the user wants to create.
-    @param int divisible_by: The integer that the user chose as part of automatically choosing a name for each of the characters.
-    @return: A list of names that have been automatically assigned.
-    """
-    names = [] # keeps a list of names.
-    
-    for i in range(len(character_list)):
+    Description:
+        Automatically names all of the characters that enter the BERYL world
         
-        # finding the number of consonants in each name.
-        count = 0
+    Parameters:
+        @param list[str] characterList: A list of predefined names that can be used to name each character
+        @param int numberOfCharacters: The number of characters that enter the BERYL world
+        @param int divisibleBy: The number that will be used to determine the names for each character
+    
+    Returns:
+        @return list[str]: A list of character names.
+    """
+    
+    names = [] # A list to store the names
+    
+    for i in range(len(characterList)):
+        
+        # Finding the number of consonants in each name
+        numOfConsonants = 0
         vowels = ['A', 'E', 'I', 'O', 'U']
-        for character in character_list[i]:
-            if character >= 'A' and character <= 'Z' and character not in vowels: # only is a consonant if it is not a number, a vowel or any special character.
-                count += 1
+        for character in characterList[i]:
+            if character >= 'A' and character <= 'Z' and character not in vowels: # Only considers ASCII values between A-Z
+                   numOfConsonants += 1
         
-        if (count % divisible_by == 0) and (character_list[i] not in names): # only append name if the number of consonants is divisible by divisible_by
-            names.append(character_list[i])
+        # If the divisibleBy divides the number of consonants for each name, append that name to the list until
+        # you reach the character limit
+        if (numOfConsonants % divisibleBy == 0) and (characterList[i] not in names):
+            names.append(characterList[i]) # We will append unique names only
         
-        if len(names) == number_of_characters: # break the loop if the number of character limit has been reached.
+        if len(names) == numberOfCharacters:
             break
-    
+        
     return names
             
+
 def return_welcome_message(names: list[str]) -> str:
     """
-    Prints a welcome message for the characters.
+    Description:
+        Returns a welcome message for the characters entering the BERYL world.
+        
+    Parameters:
+        @param list[str] names: A list of character names entering the BERYL world.
+        
+    Returns:
+        @return: A welcome message for the characters.
+    """
     
-    @param list[str] names: A list of names of the characters that the welcome message will be printed for.
-    @return: A string that consists of the welcome message.
-    """
-    if len(names) == 1: # if len() == 1
-        return ("It is the year 3045. Sentient Artificial Intelligence Bots have now taken over.\n{0} must rise from the (Mon)ashes!\nThey are BERYL's only hope!".format(names[0]))
-    else: # if len() > 1
-        return ("It is the year 3045. Sentient Artificial Intelligence Bots have now taken over.\n{0} and {1} must rise from the (Mon)ashes!\nOnly one character may survive!".format(", ".join(names[:-1]), names[-1]))
-
-#for debugging purpose only, please delete during final submission
-if __name__ == "__main__":
-    unittest.main()
-    """
-    print(set_character_number())
-    print(set_names_manual(2))
-    print(set_divided_by_integer())
-    print(set_names_automatic(DEFAULT_CHARACTER_LIST, 4, 5))
-    print(return_welcome_message(['GARY THE SNAIL', 'MICHONNE']))
-    """
+    if len(names) == 1:
+        return f"It is the year 3045. Sentient Artificial Intelligence Bots have now taken over.\n{names[0]} must rise from the (Mon)ashes!\nThey are BERYL's only hope!"
+    else:
+        return "It is the year 3045. Sentient Artificial Intelligence Bots have now taken over.\n{0} and {1} must rise from the (Mon)ashes!\nOnly one character may survive!".format(", ".join(names[:-1]), names[-1])
