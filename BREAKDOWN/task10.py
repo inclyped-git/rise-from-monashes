06f34b0266b831baa38b3411cd6cff09c98b2d7d
inclyped <shlolitsme@gmail.com>
2024-02-23 13:46:38 +0700

    Update task10.py
    Updated Task 10 to match the checkpoint file.

--- BREAKDOWN/task10.py
+++ BREAKDOWN/task10.py
@@ -1,131 +1,127 @@
-import unittest
+def get_funds_string( listOfBerylDollars: list[int] ) -> str:
+    """
+    Description:
+        The function will return a string format of the number of Dorabies, Doubloons, and Dusts the character has 
+        from a given list of Dorabies, Doubloons, and Dusts.
+    
+    Parameter:
+        @param list[int] listOfBerylDollars: A list of Dorabies, Doubloons, and Dusts.
+    
+    Returns:
+        @return str: A string that represents how many Dorabies, Doubloons, and Dusts the character has.
+    """
+    
+    # Extracting Dorabies, Doubloons, and Dusts from a list of [Dorabies, Doubloons, Dusts]
+    numOfDorabies = listOfBerylDollars[0]; numOfDoubloons = listOfBerylDollars[1]; numOfDusts = listOfBerylDollars[2]
+    
+    # Need to print x Dorabies if numOfDorabies == 0 or numOfDorabies > 1, otherwise print 1 Dorabie
+    hasMultipleDorabies = "Dorabie" if ( numOfDorabies == 1 ) else "Dorabies"
+    
+    # Same concept applied to Doubloons and Dusts
+    hasMultipleDoubloons = "Doubloon" if ( numOfDoubloons == 1 ) else "Doubloons"
+    hasMultipleDusts = "Dust" if ( numOfDusts == 1 ) else "Dusts"
+    
+    return f"{numOfDorabies} {hasMultipleDorabies}, {numOfDoubloons} {hasMultipleDoubloons} and {numOfDusts} {hasMultipleDusts}"
 
-class TestAllFunctions(unittest.TestCase):
-    
-    def test_funds_string(self):
-        self.assertEqual(get_funds_string([4,5,19]), "4 Dorabies, 5 Doubloons and 19 Dusts")
-        self.assertEqual(get_funds_string([3,0,2]), "3 Dorabies, 0 Doubloons and 2 Dusts")
-        self.assertEqual(get_funds_string([1,2,3]), "1 Dorabie, 2 Doubloons and 3 Dusts")
-        self.assertEqual(get_funds_string([5,6,88]), "5 Dorabies, 6 Doubloons and 88 Dusts")
-        self.assertEqual(get_funds_string([4,1,77]), "4 Dorabies, 1 Doubloon and 77 Dusts")
-    
-    def test_conversion(self):
-        self.assertEqual(beryl_dollar_conversion(540,20,5), [5, 2, 0])
-        self.assertEqual(beryl_dollar_conversion(123,32,15), [0, 3, 27])
-        self.assertEqual(beryl_dollar_conversion(111,10,5), [2, 1, 1])
-        self.assertEqual(beryl_dollar_conversion(387,28,6), [2, 1, 23])
-        self.assertEqual(beryl_dollar_conversion(15543,255,11), [5, 5, 243])
+def beryl_dollar_conversion( amountOfDusts: int, dustToDoubloonRate: int, doubloonToDorabieRate: int ) -> list[int]:
+    """
+    Description:
+        The function will return a list of Dorabies, Doubloons, and Dusts after converting all of the Dusts using
+        the exchange rates used in BERYL world.
         
-    def test_indicator(self):
-        self.assertEqual(beryl_dollar_indicator(152, [0,3,5], 10, 5), [2, 3, 7])
-        self.assertEqual(beryl_dollar_indicator(-22, [0,3,5], 10, 5), [0, 0, 0])
-        self.assertEqual(beryl_dollar_indicator(542, [5,10,40], 30, 10), [11, 5, 12])
-        self.assertEqual(beryl_dollar_indicator(-32, [10,20,33], 12, 5), [11, 0, 9])
-        self.assertEqual(beryl_dollar_indicator(-89, [1,6,78], 10, 5), [0,0,0])
-         
-
-def get_funds_string(listOfBerylDollars: list[int]) -> str:
-    """
-    Returns the number of Dorabies, Doubloons, and Dusts the user has, from a given list of dollars in a string format.
+    Parameters:
+        @param int amountOfDusts: The number of Dusts the character will have to convert to corresponding Dorabies, 
+                                Doubloons, and remaining Dusts.
+        @param int dustToDoubloonRate: The conversion rate of converting x Dusts to 1 Doubloon.
+        @param int doubloonToDorabieRate: The conversion rate of converting x Doubloons to 1 Dorabie.
     
-    @param list[int] listOfBerylDollars: List of dollars representing respective Dorabies, Doubloons, and Dusts.
-    @return: Representable string that represents Dorabies, Doubloons, and Dusts that the user has.
+    Returns:
+        @return list[int]: A list that contains the converted Dorabies, Doubloons and remaining Dusts from the initial
+                    pack of Dusts alone.
     """
-    # extracting dorabies, doubloons, and dusts and storing them in appropriate variables.
-    numOfDorabies = listOfBerylDollars[0] 
-    numOfDoubloons = listOfBerylDollars[1]
-    numOfDusts = listOfBerylDollars[2] 
-
-    # manipulates the output string depending on the number of dollar units the user has. 
-    hasMultipleDorabies = "Dorabies" if ( numOfDorabies > 1 or numOfDorabies == 0) else "Dorabie"
-    hasMultipleDoubloons = "Doubloons" if ( numOfDoubloons > 1 or numOfDoubloons == 0) else "Doubloon"
-    hasMultipleDusts = "Dusts"  if ( numOfDusts > 1 or numOfDusts == 0) else "Dust"
-
-    # returning the final output of the dollar units.
-    return ("{0} {1}, {2} {3} and {4} {5}".format(numOfDorabies, hasMultipleDorabies, numOfDoubloons, hasMultipleDoubloons, numOfDusts, hasMultipleDusts))
     
+    # Directly using the two exchange rates to directly convert x Dusts to y Dorabies
+    dustToDorabieRate = dustToDoubloonRate * doubloonToDorabieRate
     
-def beryl_dollar_conversion(amountOfDusts: int, dustToDoubloon_rate: int, doubloonToDorabies_rate: int) -> list[int]:
-    """
-    Returns a list of converted Dorabies, Doubloons, and Dusts from a given number of Dusts
+    # Converting all of the dusts into Dorabies, Doubloons, and Dusts
+    convertedDorabies = int ( amountOfDusts / dustToDorabieRate )
+    remainingDusts = amountOfDusts - convertedDorabies * dustToDorabieRate # Remaining Dusts after converting some to Dorabies
     
-    @param int amountOfDusts: Takes in the number of Dusts the user has.
-    @param int dustToDoubloon_rate: The rate of conversion from Dusts to Doubloons.
-    @param int doubloonToDorabies_rate: The rate of conversion from Doubloons to Dorabies.
-    @return: A list of converted Dorabies, Doubloons, and Dusts.
-    """
-    dustToDorabie_rate = (dustToDoubloon_rate * doubloonToDorabies_rate) # getting the dust to dorabie rate for direct conversion of dust to dorabies.
-
-    # converting dusts to dorabies and remaining dusts.
-    convertedDorabies = int ( amountOfDusts / dustToDorabie_rate )
-    remainingDusts = amountOfDusts - ( convertedDorabies * dustToDorabie_rate )
-
-    # converting remaining dusts to doubloons and final dusts.
-    convertedDoubloons = int ( remainingDusts / (dustToDoubloon_rate) )
-    convertedDusts = remainingDusts - ( convertedDoubloons * dustToDoubloon_rate )
-
-    # returning a list of converted dollar units.
+    convertedDoubloons = int ( remainingDusts / dustToDoubloonRate )
+    
+    convertedDusts = remainingDusts - convertedDoubloons * dustToDoubloonRate # Remaining dusts will be equal to converted Dusts
+                                                                            # The others are converted to Doubloons
+    
     return [convertedDorabies, convertedDoubloons, convertedDusts]
     
 
-def beryl_dollar_indicator(dollarIndicator: int, startingFundsList: list[int], dustsToDoubloons_rate: int, doubloonsToDorabies_rate: int) -> list[int]:
+def beryl_dollar_indicator( dollarIndicator: int, startingFundsList: list[int], dustToDoubloonRate: int, doubloonToDorabieRate: int ) -> list[int]:
     """
-    Returns a list of Dorabies, Doubloons, and Dusts remaining after encountering with the Dollar Indicator.
-    
-    @param int dollarIndicator: The Dollar Indicator
-    @param list[int] startingFundsList: The starting Dorabies, Doubloons, Dusts before encountering with the Dollar Indicator.
-    @param int dustToDoubloon_rate: The rate of conversion from Dusts to Doubloons.
-    @param int doubloonToDorabies_rate: The rate of conversion from Doubloons to Dorabies.
-    @return: A list of final Dorabies, Doubloons, Dusts after encountering the Dollar Indicator.
+    Description:
+        The function will return a list of remaining Dorabies, Doubloons, and Dusts after the character
+        encounters with the Dollar Indicator.
+        
+    Parameters:
+        @param int dollarIndicator: The Dollar Indicator that will be encountered by the character.
+        @param list[int] startingFundsList: The starting Dorabies, Doubloons, and Dusts before encountering the
+                                        Dollar Indicator.
+        @param int dustToDoubloonRate: The rate of conversion from x Dusts to 1 Doubloon.
+        @param int doubloonToDorabieRate: The rate of conversion from x Doubloons to 1 Dorabie.
+        
+    Returns:
+        @return list[int]: A list of remaining Dorabies, Doubloons, and Dusts after encountering the Dollar Indicator.
     """
     
-    # converting dollarIndicator into dusts
-    totalDusts = 0
-    if (dollarIndicator> 0): # if dollar indicator is positive.
-        
-        # first digit of the indicator is the dorabie amount added to the initial dorabie amount.
-        convertedDorabies = int( dollarIndicator / 100 )
-        remainingDollarIndicator = dollarIndicator - ( convertedDorabies * 100 ) # remaining dollar indicators is calculated.
-        
-        # second digit of the indicator is the doubloon amount added to the initial doubloon amount.
-        convertedDoubloons = int ( remainingDollarIndicator / 10 )
-        remainingDollarIndicator = remainingDollarIndicator - convertedDoubloons * 10 # remaining dollar indidcators is calculated.
-
-        totalDusts = remainingDollarIndicator + ( convertedDoubloons * dustsToDoubloons_rate ) + ( convertedDorabies * ( dustsToDoubloons_rate * doubloonsToDorabies_rate ) ) # convert all of the converted units back to dusts.
-
-    else: # if dollar indicator is negative.
-        
-        # first digit of the indicator is the dorabie amount going to be subtracted from the starting dorabie amount.
-        convertedDorabies = int ( dollarIndicator / 10 )
-        remainingDollarIndicator = dollarIndicator - ( convertedDorabies * 10 ) # remaining dollar indicators is calculated.
-        
-        totalDusts = ( remainingDollarIndicator * dustsToDoubloons_rate ) + ( convertedDorabies * (dustsToDoubloons_rate * doubloonsToDorabies_rate) ) # convert all of the converted units back to dusts.
-        
-    # getting the starting dorabie, doubloon, and dust amounts from the list startingFundsList[]
-    startingDorabies = startingFundsList[0]
-    startingDoubloons = startingFundsList[1]
-    startingDusts = startingFundsList[2]
-
-    # converting starting dorabies, doubloons, dusts all to dusts.
-    startingTotalDusts = startingDorabies * (dustsToDoubloons_rate * doubloonsToDorabies_rate)
-    startingTotalDusts += startingDoubloons * (dustsToDoubloons_rate)
-    startingTotalDusts += startingDusts
-
-    # adding the starting dust amount with the dusts converted from the dollar indicator.
-    finalTotalDusts = startingTotalDusts + totalDusts
-
-    # converting the final dusts back to dorabies, doubloons, and remaining dusts
-    # if dorabies, doubloons and dusts are less than 0, it is automatically defined to 0.
-    finalDorabies = int ( finalTotalDusts / (dustsToDoubloons_rate * doubloonsToDorabies_rate) ) if int ( finalTotalDusts / (dustsToDoubloons_rate * doubloonsToDorabies_rate) ) > 0 else 0
-    remainingFinalDusts = finalTotalDusts - ( finalDorabies *  (dustsToDoubloons_rate * doubloonsToDorabies_rate) ) # remaning dusts after dorabie conversion
-
-    finalDoubloons = int ( remainingFinalDusts / (dustsToDoubloons_rate) ) if int ( remainingFinalDusts / (dustsToDoubloons_rate) ) > 0 else 0
+    # Converting the starting funds all into Dusts
+    # Directly converting from Dorabies to Dusts using the two exchange rates
+    dustToDorabieRate = dustToDoubloonRate * doubloonToDorabieRate
     
-    finalDusts = remainingFinalDusts - ( finalDoubloons * dustsToDoubloons_rate ) # remaning dusts after doubloon conversion
-    finalDusts = finalDusts if finalDusts > 0 else 0
+    startingFundsInDust = startingFundsList[0] * dustToDorabieRate
+    startingFundsInDust += startingFundsList[1] * dustToDoubloonRate
+    startingFundsInDust += startingFundsList[2]
+    
+    # converting the Dollar Indicator into Dusts
+    dollarIndicatorInDust = 0
+    if ( dollarIndicator > 0 ): # if the Dollar Indicator is positive
+        
+        # First digit of the indicator implies the Dorabie amount is added to the funds
+        dollarIndicatorDorabies = int ( dollarIndicator / 100 )
+        remainingDollarIndicator = dollarIndicator - ( dollarIndicatorDorabies * 100 )
+        
+        # Second digit of the indicator implies the Doubloon amount is added to the funds
+        dollarIndicatorDoubloons= int ( remainingDollarIndicator / 10 )
+        
+        # The remainder is the third digit of the indicator, implying that the Dust amount is added to the funds
+        dollarIndicatorDusts = remainingDollarIndicator - ( dollarIndicatorDoubloons * 10 )
+        
+        dollarIndicatorInDust = dollarIndicatorDusts + dollarIndicatorDoubloons * dustToDoubloonRate + dollarIndicatorDorabies * dustToDorabieRate
+    
+    elif ( dollarIndicator < 0 ): # if the Dollar Indicator is negative
+        
+        # First digit of the indicator implies the Dorabie amount is subtracted from the funds
+        dollarIndicatorDorabies = int ( dollarIndicator / 10 )
+        
+        # The remainder is the second digit of the indicator, implying the Doubloon amount is subtracted from the funds
+        dollarIndicatorDoubloons = dollarIndicator - ( dollarIndicatorDorabies * 10 )
+        
+        dollarIndicatorInDust = dollarIndicatorDorabies * dustToDorabieRate + dollarIndicatorDoubloons * dustToDoubloonRate
+    
+    finalFundsInDust = startingFundsInDust + dollarIndicatorInDust
+    
+    # Converting the final Dusts into Dorabies, Doubloons, and remaining Dusts
+    # If any of the dollar units are less than zero, they are automatically defined to zero
+    finalDorabies = int ( finalFundsInDust / dustToDorabieRate ) if int ( finalFundsInDust / dustToDorabieRate ) > 0 else 0
+    remainingFinalDusts = finalFundsInDust - finalDorabies * dustToDorabieRate
+    
+    finalDoubloons = int ( remainingFinalDusts / dustToDoubloonRate ) if int ( remainingFinalDusts / dustToDoubloonRate ) > 0 else 0
+    
+    finalDusts = remainingFinalDusts - finalDoubloons * dustToDoubloonRate if remainingFinalDusts - finalDoubloons * dustToDoubloonRate > 0 else 0
+    
     
     return [finalDorabies, finalDoubloons, finalDusts]
 
 #for debugging purpose only, please delete during final submission
 if __name__ == "__main__":
-    unittest.main()
+    print(get_funds_string([4,5,19]))
+    print(beryl_dollar_conversion(540, 20, 5)) 
+    print(beryl_dollar_indicator(152, [0,3,5], 10, 5))
